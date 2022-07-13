871c9e4122bbbd374daed601aa1d6ec740a16332
Max Baravy <maximkatut@gmail.com>
2022-07-13 12:34:47 -0400

    add custom colors tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,28 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      black: "#000112",
+      grey: {
+        light: "#F4F7FD",
+        medium: "#828FA3",
+        dark: "#2B2C37",
+        ["very-dark"]: "#20212C",
+      },
+      lines: {
+        dark: "#3E3F4E",
+        light: "#E4EBFA",
+      },
+      white: "#FFFFFF",
+      purple: {
+        DEFAULT: "#635FC7",
+        hover: "#A8A4FF",
+      },
+      red: {
+        DEFAULT: "#EA5555",
+        hover: "#FF9898",
+      },
+    },
     extend: {},
   },
   plugins: [],
