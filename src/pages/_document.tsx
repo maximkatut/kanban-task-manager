bc6955674b1368ae6824ac83e27a46a7fc26bcc5
Max Baravy <maximkatut@gmail.com>
2022-07-14 16:58:03 -0400

    local storage dark theme handling

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,10 +6,10 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <Script id="dark-mode" strategy="lazyOnload">
-          {`if (localStorage.getItem(' ') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark');
-          } else {
-            document.documentElement.classList.remove('dark')
+          {`if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+              document.documentElement.classList.add('dark')
+            } else {
+              document.documentElement.classList.remove('dark')
           }`}
         </Script>
       </Head>
