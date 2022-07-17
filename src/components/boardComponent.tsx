a550bd7d41d00ae6c0c02031454cd5e98b3701d0
Max Baravy <maximkatut@gmail.com>
2022-07-16 21:27:33 -0400

    render columns from db

--- src/components/boardComponent.tsx
+++ src/components/boardComponent.tsx
@@ -1,15 +1,17 @@
 import { Board } from "@prisma/client";
+import { trpc } from "../utils/trpc";
 import Column from "./column";
 interface BoardProps {
-  board: Board | undefined;
+  board: Board;
 }
 
 const Board = ({ board }: BoardProps) => {
+  const { data: columns } = trpc.useQuery(["column.getByBoardId", { boardId: board.id }]);
   return (
     <ul className="min-h-[calc(100vh-80rem)] p-5 flex">
-      <Column />
-      <Column />
-      <Column />
+      {columns?.map((c) => {
+        return <Column key={c.id} column={c} />;
+      })}
       <li
         onClick={() => {}}
         className="group w-[260px] h-[calc(100vh-200px)] mt-[43px] mb-[19px] rounded-lg bg-grey-create dark:bg-grey-create-dark hover:bg-purple-10 flex flex-col justify-center items-center cursor-pointer"
