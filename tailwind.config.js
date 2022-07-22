f82e0dd4b6ed0379214150721a95c4023360f866
Max Baravy <maximkatut@gmail.com>
2022-07-22 15:45:40 -0400

    extend tw config with shake animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,27 @@ module.exports = {
       blue: "#49C4E5",
       green: "#67E2AE",
     },
-    extend: {},
+    extend: {
+      animation: {
+        shake: "shake 1s ease-in-out",
+      },
+      keyframes: {
+        shake: {
+          "10%, 90%": {
+            transform: "translate3d(-1px, 0, 0)",
+          },
+          "20%, 80%": {
+            transform: "translate3d(2px, 0, 0)",
+          },
+          "30%, 50%, 70%": {
+            transform: "translate3d(-4px, 0, 0)",
+          },
+          "40%, 60%": {
+            transform: "translate3d(4px, 0, 0)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
